070fefa5960fb822ad236da2c7777951a58a6d19
André <andreocs.dev@gmail.com>
2026-03-11 20:56:25 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import "dotenv/config";
 
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { bible: "checked", lesson: "checked" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
