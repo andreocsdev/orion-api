cd7f0a26bb5b154a2ba991a0e189cd5029e68a12
André <andreocs.dev@gmail.com>
2026-03-12 11:10:13 -0300

    chore: add fastify swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import z from "zod";
 import Fastify from "fastify";
+import { url } from "inspector";
 
 const app = Fastify({
   logger: true,
@@ -14,10 +18,33 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "ÓrionApi",
+      description: "API para o Site do Ministério Órion",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
   schema: {
+    description: "Rota de teste para verificar se a API está funcionando",
+    tags: ["Bíblia e Lição"],
     response: {
       200: z.object({
         bible: z.string(),
@@ -27,7 +54,7 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
   handler: () => {
     return { bible: "checked", lesson: "checked" };
-  }
+  },
 });
 
 try {
